3713aceea1e8b5cf130273c16f7dad2f0f1274b1
Claes Svensson <a19clasv@student.his.se>
2022-05-16 19:47:58 +0200

    Used external library "FontometricsLibrary" to change the font of the TextView in main layout

--- app/src/main/java/com/example/externallibraries/MainActivity.java
+++ app/src/main/java/com/example/externallibraries/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.externallibraries;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.fontometrics.Fontometrics;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.text);
+        textView.setTypeface(Fontometrics.elephant(this));
     }
 }
